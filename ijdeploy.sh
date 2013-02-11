2f62b2a572575d240fa78c8112f64e780bb09143
Ivan Dubrov <idubrov@guidewire.com>
2013-02-11 11:20:39 -0800

    Fixing groupId for SDK artifact

--- ijdeploy.sh
+++ ijdeploy.sh
@@ -42,7 +42,7 @@ cat > ${SDKPOM} <<EOF
 <project xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd" xmlns="http://maven.apache.org/POM/4.0.0"
     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
   <modelVersion>4.0.0</modelVersion>
-  <groupId>com.jetbrains.intellij</groupId>
+  <groupId>com.jetbrains.intellij.platform</groupId>
   <artifactId>sdk</artifactId>
   <version>${VERSION}</version>
   <description>${DESC}</description>
